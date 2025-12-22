9b4c625d4bef3e90a864a434c1974689b977cd81
ahmed <ahmedkinga60@gmail.com>
2025-12-22 02:06:33 +0200

    fix camp

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"1425e5e9ec5eeb4f225c401d8db69b860e0fde
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1930425084"
+    serviceWorkerVersion: "2466884128"
   }
 });
