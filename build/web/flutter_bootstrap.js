94a5bb292e215dc6037ea3087558b012dbf8c961
ahmed <ahmedkinga60@gmail.com>
2025-12-22 11:22:29 +0200

    fix price

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"1425e5e9ec5eeb4f225c401d8db69b860e0fde
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2466884128"
+    serviceWorkerVersion: "2885290693"
   }
 });
