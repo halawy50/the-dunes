f5a99cd13f89ea3ad820d6be71d2a8ec8473e5f8
Omar <omarabdullah.clevernessagency@gmail.com>
2026-01-11 15:28:04 +0200

    update version

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -70,7 +70,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "31b418da4c23be73d0e5cef109e3ed74
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "6c875f3dec8e308d7865b8d51c06adcb",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "8c0cfcb4c95f4bdcd0c953d72d66f7dc",
+"flutter_bootstrap.js": "c279f12f11cfeb2c85f6a7afef15349f",
 "icons/Icon-192.png": "6c875f3dec8e308d7865b8d51c06adcb",
 "icons/Icon-512.png": "6c875f3dec8e308d7865b8d51c06adcb",
 "icons/Icon-maskable-192.png": "6c875f3dec8e308d7865b8d51c06adcb",
